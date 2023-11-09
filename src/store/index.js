d5bba2f4a3eee6326fd45be01a09eab95aca3ecd
clintjeff2 <clintjeff2@gmail.com>
2023-11-09 04:44:50 +0100

    Created the student slice in redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import staffReducer from './staffs/staffSlice';
 import studentReducer from './students/studentSlice';
+import specialtyReducer from './specialty/specialtySlice';
+
 const store = configureStore({
 	reducer: {
 		staffs: staffReducer,
 		students: studentReducer,
+		specialty: specialtyReducer,
 	},
 });
 
--- src/store/students/studentSlice.js
+++ src/store/students/studentSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import getApiError from '../../utilities/getApiError';
 
 const initialState = {
-	students: {},
+	students: [],
 	error: false,
 	isLoading: false,
 };
@@ -20,6 +20,59 @@ export const getStudents = createAsyncThunk(
 			return res.data;
 		} catch (err) {
 			// const msg = getApiError();
+			return thunkAPI.rejectWithValue({ error: err.message });
+		}
+	}
+);
+
+export const addStudent = createAsyncThunk(
+	'student/addStudent',
+	async (
+		{
+			name,
+			matricule,
+			specialty,
+			address,
+			gender,
+			dob,
+			pod,
+			email,
+			tel,
+			parent_name,
+			parent_email,
+			parent_tel,
+			level,
+			entry_certificate,
+		},
+		thunkAPI
+	) => {
+		try {
+			console.log(name, matricule);
+			const res = await axios({
+				method: 'post',
+				url: 'http://localhost:8001/api/v1/student',
+				data: {
+					name,
+					matricule,
+					specialty,
+					address,
+					gender,
+					dob,
+					pod,
+					email,
+					tel,
+					parent_name,
+					parent_email,
+					parent_tel,
+					level,
+					entry_certificate,
+				},
+			});
+
+			console.log(res);
+			return res.data;
+		} catch (err) {
+			console.log(err);
 			return thunkAPI.rejectWithValue({ error: err.message });
 		}
 	}
@@ -42,6 +95,17 @@ const studentSlice = createSlice({
 			})
 			.addCase(getStudents.fulfilled, (state, action) => {
 				state.students = action.payload.data;
+				state.isLoading = false;
+			})
+			.addCase(addStudent.fulfilled, (state, action) => {
+				// state.students = action.payload.data;
+				state.isLoading = false;
+			})
+			.addCase(addStudent.pending, (state, action) => {
+				state.isLoading = true;
+			})
+			.addCase(addStudent.rejected, (state, action) => {
+				state.error = true;
 				state.isLoading = false;
 			});
 	},
