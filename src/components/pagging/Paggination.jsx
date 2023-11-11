cc0b456d0cd713a57f67b373e5249224007b55e8
clintjeff2 <clintjeff2@gmail.com>
2023-11-11 22:22:57 +0100

    Added functionality for navigating students information by pagginations

--- src/components/pagging/Paggination.jsx
+++ src/components/pagging/Paggination.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
 
-//Styled in the pagin sass file in the component section
-function Paggination({ styles }) {
+//Styled in the paging sass file in the component section
+function Paggination({ styles, paggingNum, totalData }) {
+	const tabs = Math.ceil(totalData / paggingNum);
+	const [searchParams] = useSearchParams();
+	let cur = Number(searchParams.get('curPage') || 1);
+	const curMaxEntries = cur * paggingNum;
 	return (
 		<div className={`pagging ${styles ? styles : ''}`}>
-			<span className="text">Showing x to X of Y entries</span>
+			<span className="text">
+				Showing {(cur - 1) * paggingNum + 1} to{' '}
+				{curMaxEntries > totalData ? totalData : curMaxEntries} of {totalData}{' '}
+				entries
+			</span>
 			<div className="pagging-links">
-				<NavLink to="?start=x&end=y">Prev</NavLink>
-				<NavLink to="?start=x&end=y">1</NavLink>
-				<NavLink to="?start=x&end=y">2</NavLink>
-				<NavLink to="?start=x&end=y">3</NavLink>
-				<NavLink to="?start=x&end=y">4</NavLink>
-				<NavLink to="?start=x&end=y">Next</NavLink>
+				{cur > 1 && <NavLink to={`?curPage=${cur - 1}`}>Prev</NavLink>}
+
+				{Array.from({ length: tabs }).map((_, index) => {
+					return (
+						<NavLink
+							to={`?curPage=${index + 1}`}
+							className={`${cur === index + 1 ? 'page-active' : ''}`}
+							key={index}
+						>
+							{index + 1}
+						</NavLink>
+					);
+				})}
+
+				{cur < tabs && <NavLink to={`?curPage=${cur + 1}`}>Next</NavLink>}
 			</div>
 		</div>
 	);
--- src/components/pagging/PaggingNumSelect.jsx
+++ src/components/pagging/PaggingNumSelect.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 //Select number of items to be shown on table
-function PaggingNumSelect() {
-  return (
-    <div className='entries'>
-      <span className="text">Show</span>
-      <select name="show-entries" id="entries">
-        <option value="10">10</option>
-        <option value="25">25</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
-      </select>
-      <span className="text">entries</span>
-    </div>
-  )
+function PaggingNumSelect({ setItemsPerPage }) {
+	return (
+		<div className="entries">
+			<span className="text">Show</span>
+			<select
+				name="show-entries"
+				id="entries"
+				onChange={(e) => setItemsPerPage(e.target.value)}
+			>
+				<option value="5">5</option>
+				<option value="10">10</option>
+				<option value="25">25</option>
+				<option value="50">50</option>
+				<option value="75">75</option>
+				<option value="100">100</option>
+			</select>
+			<span className="text">entries</span>
+		</div>
+	);
 }
 
 export default PaggingNumSelect;
