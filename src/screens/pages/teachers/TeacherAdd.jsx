317efc6739f88ec799324fff7b678c54060998cd
clintjeff2 <clintjeff2@gmail.com>
2023-11-11 22:32:22 +0100

    Impleted the registering of teachers and getting their information from the database. Also, implemented the searching of teachers by department and by name. And lastly, the sorting of teachers on any criteria in ascending order

--- src/screens/pages/teachers/TeacherAdd.jsx
+++ src/screens/pages/teachers/TeacherAdd.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, SectionIntro } from '../../../components/layout';
 import { TeacherForm } from '../../../components/form';
 
 function TeacherAdd() {
 	return (
+		// Application layout
 		<Layout>
 			<SectionIntro title="Add Teachers" main="Teacher" sub="add" />
 			<section className="teachers mg-top">
 				<h2 className="header-secondary">Teacher Information</h2>
+
+				{/* Form for the creation of teachers by the admin */}
 				<TeacherForm />
 			</section>
 		</Layout>
--- src/screens/pages/teachers/TeacherList.jsx
+++ src/screens/pages/teachers/TeacherList.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getStaffs } from '../../../store/staffs/staffSlice';
 
 import {
@@ -9,23 +9,51 @@ import {
 } from '../../../components/layout/';
 import { PaggingNumSelect, Paggination } from './../../../components/pagging/';
 import SearchCategory from '../../../components/search/SearchCategory';
-// import { StandardTable } from '../../../components/tables/';
+import { TableStaff } from '../../../components/tables/';
+
+//initializing table header information
+const staffHeader = {
+	id: 'Matricule',
+	name: 'Name',
+	email: 'email',
+	gender: 'gender',
+	role: 'role',
+	dob: 'DoB',
+	tel: 'phone Number',
+	acts: 'actions',
+	department: 'department',
+	address: 'address',
+};
 
 function TeacherList() {
-	// const staffs = useSelector((state) => state.staffs);
+	const staffs = useSelector((state) => state.staffs.teachers.data);
 	const dispatch = useDispatch();
 
+	const [staffsData, setStaffsState] = useState([]);
+	// console.log(staffs !== undefined ? staffs : 'qqq', 1234);
+	console.log(staffs, staffsData, 'DAT');
 	useEffect(() => {
 		dispatch(getStaffs());
 	}, [dispatch]);
 	return (
 		<Layout>
 			<SectionIntro title="Teachers" main="Teacher" sub="List" />
-			<SearchCategory styles={'mg-top-md mg-bt-md'} />
+			<SearchCategory
+				styles={'mg-top-md mg-bt-md'}
+				dropDown="department"
+				data={staffs}
+				setData={setStaffsState}
+			/>
 			<section className="teachers">
 				<SectionMainIntro title="Teachers" styles="mg-bt" />
 				<PaggingNumSelect />
-				{/* <StandardTable styles="mg-top" data={staffs} /> */}
+				{staffs !== undefined && (
+					<TableStaff
+						styles="mg-top"
+						tableData={staffsData.length !== 0 ? staffsData : staffs}
+						header={staffHeader}
+					/>
+				)}
 				<Paggination styles="mg-top" />
 			</section>
 		</Layout>
